5ad2d4374d8cc92d060fdc1611bd2e2beac145e7
Muchiri-cmd <davismuchiri21@gmail.com>
2024-07-19 07:33:43 +0300

    Add backend - database functionality & error handling

--- index.js
+++ index.js
@@ -48,15 +48,35 @@ app.get('/api/notes',(req,res)=>{
     Note.find({}).then(notes=>res.json(notes))
 })
 
-app.get('/api/notes/:id',(req,res)=>{
-    Note.findById(req.params.id).then(note=>res.json(note))
+app.get('/api/notes/:id',(req,res,next)=>{
+    Note.findById(req.params.id).then(note=>{
+        if (note){
+            res.json(note)
+        }else {
+            res.status(404).end()
+        }
+    }).catch(err => next(err))
 })
 
-app.delete('/api/notes/:id',(req,res)=>{
-    const id = req.params.id
-    notes = notes.filter(note=> note.id!=id )
-    res.status(204).end()
+app.delete('/api/notes/:id',(req,res,next)=>{
+    Note.findByIdAndDelete(req.params.id)
+        .then(result=>res.status(204).end())
+        .catch(err => next(err))
 })
+
+app.put('/api/notes/:id',(req,res,next)=>{
+    const body = req.body
+    const note = {
+        content : body.content,
+        important: body.important
+    }
+
+    Note.findByIdAndUpdate(req.params.id,note,{new:true})
+        .then(updatedNote=>res.json(updatedNote))
+        .catch (err => next(err))
+
+})
+
 app.post('/api/notes',(req,res)=>{
     const body = req.body 
 
@@ -72,12 +92,20 @@ app.post('/api/notes',(req,res)=>{
     note.save().then(savedNote=>res.json(savedNote))
 })
 
-
 const unknownEndPoint = (req,res)=>{
     res.status(404).send({error:'unknown endpoint'})
 }
-
 app.use(unknownEndPoint)
+
+const errorHandler = (err,req,res,next) =>{
+    console.log(err.message)
+
+    if (err.name === 'CastError'){
+        return res.status(400).send({error:'malformtted id'})
+    }
+    next(err)
+}
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT,()=>{
